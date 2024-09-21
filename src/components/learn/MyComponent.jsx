c2cf10b77be78541a408cd867f34fa3929ea40d7
HuyLuong <luonghuy131001@gmail.com>
2024-09-21 23:12:24 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,20 @@ import "./style.css";
 
 //component = html + css + js
 const MyComponent = () => {
+  const name = "HuyLuong"; //string
+  const number = 23; //number
+  const textBoolean = true; //boolean
+  //   const check = undefined; //! underfined
+  const check = null;
+  const checkArray = [1, 2, 3]; //array
+  const checkObject = {
+    name: "Cati",
+    age: 25,
+  };
   return (
     <>
-      <div>Huy & Reactjs</div>
+      <div>{JSON.stringify(checkObject)} & Reactjs</div>
+      <div>{console.log("HuyLuong")}</div>
       <div
         className="child"
         style={{
