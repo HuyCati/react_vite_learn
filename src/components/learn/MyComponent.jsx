ece954bbe545a314b85a50d4e3f37b8a70243063
HuyLuong <luonghuy131001@gmail.com>
2024-09-21 21:58:32 +0700

    video 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,6 @@
+//component = html + css + js
+const MyComponent = () => {
+  return <div>Huy & Reactjs Update</div>;
+};
+
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,9 @@
+const SecondComponent = () => {
+  return <div>second component</div>;
+};
+
+const ThirdComponent = () => {
+  return <div>Third Component</div>;
+};
+
+export { SecondComponent, ThirdComponent };
