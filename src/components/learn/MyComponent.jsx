243699b5ea0ea0f67eceddce78c1dadd4bbe9354
HuyLuong <luonghuy131001@gmail.com>
2024-09-21 22:15:12 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,20 @@
+import "./style.css";
+
 //component = html + css + js
 const MyComponent = () => {
-  return <div>Huy & Reactjs Update</div>;
+  return (
+    <>
+      <div>Huy & Reactjs</div>
+      <div
+        className="child"
+        style={{
+          borderRadius: "10px",
+        }}
+      >
+        child
+      </div>
+    </>
+  );
 };
 
 export default MyComponent;
